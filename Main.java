0a4e3d6083b7ee57cc51a9f7cdf020ae2a7c7593
Karma63 <adam2004buchan@outlook.com>
2023-12-23 09:28:31 +0000

    Refactor Main class and improve code formatting

--- Main.java
+++ Main.java
@@ -1,23 +1,29 @@
 
 /**
- * A class to initalise the times array and then pass to the method in support 
+ * A class to initalise the times array and then pass to the method in support
  * and then calling the Equations function in support to get the results
+ * 
  * @author Adam Buchan
  * @version 14th of march 2022
  */
-public class Main
-{
-    //a function to intailse the array and pass the array the call a function
-    //to get the output
-    public static void main(String[] args)
-    {
-        //iniatlising the array
-        double[] times = {21.23,23.45,23.98,22.22,24.12,21.23,21.23,21.45};
-        
-        //passing the array to the method in support
+public class Main {
+    // a function to intailse the array and pass the array the call a function
+    // to get the output
+    public static void main(String[] args) {
+        // iniatlising the array
+        double[] times = { 21.23, 23.45, 23.98, 22.22, 24.12, 21.23, 21.23, 21.45 };
+
+        for (double temp : times) {
+            // print out array
+            System.out.print(temp + " ");
+        }
+
+        System.out.println("");
+
+        // passing the array to the method in support
         Support passing = new Support(times);
-        
-        //calling function to get the ouput
+
+        // calling function to get the ouput
         passing.Equation();
-    }   
+    }
 }
